8f328649fe6f0ecb45bd891072a7f2fa175f6be6
NothJosue <thedarkm04@gmail.com>
2025-08-07 12:37:30 -0500

    Proyecto terminado

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,9 @@
-import service.WheaterService;
 import view.Menu;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        //WheaterService wheaterService=new WheaterService();
-        //System.out.println(wheaterService.findWheater("new jersey"));
         Scanner inputUser = new Scanner(System.in);
         Menu.viewInterfaceMenu(inputUser);
     }
--- src/main/java/controller/MatchActivity.java
+++ src/main/java/controller/MatchActivity.java
@@ -5,29 +5,18 @@ import model.Wheater;
 
 public class MatchActivity {
 
-    public ActivityTipeEnum matchingActivity(Wheater wheater){
+    public ActivityTipeEnum matchingActivity(Wheater wheater) {
 
-        String weatherDescription=wheater.getDescriptions();
+        String weatherDescription = wheater.getDescriptions();
 
-        if (weatherDescription.equals("Sunny")||weatherDescription.equals("clear")){
-
-            return ActivityTipeEnum.recreational;
-
-        } else if (weatherDescription.equals("Partly cloudy")) {
-            return ActivityTipeEnum.social;
-        } else if (weatherDescription.equals("Light rain")||weatherDescription.equals("Light rain shower")||weatherDescription.equals("Moderate rain")||weatherDescription.equals("Patchy rain nearby")) {
-            return ActivityTipeEnum.education;
-        } else if (weatherDescription.equals("Mist")||weatherDescription.equals("Haze")) {
-
-            return ActivityTipeEnum.relaxation;
-
-        } else if (weatherDescription.equals("Overcast")) {
-
-            return ActivityTipeEnum.busywork;
-        }else {
-            throw new RuntimeException("Weather description not exist");
-        }
+        return switch (weatherDescription) {
+            case "Sunny", "Clear" -> ActivityTipeEnum.recreational;
+            case "Partly cloudy" -> ActivityTipeEnum.social;
+            case "Light rain", "Light rain shower", "Moderate rain", "Patchy rain nearby" -> ActivityTipeEnum.education;
+            case "Mist", "Haze" -> ActivityTipeEnum.relaxation;
+            case "Overcast" -> ActivityTipeEnum.busywork;
+            default -> throw new RuntimeException("Weather description not exist");
+        };
 
     }
-
 }
